58ec62c171bbd2e320061d7c4e2ab4ae628e9206
Discover- <amort11@hotmail.com>
2013-08-21 18:51:44 +0200

    Push initial Drag-N-Drop source.

---
+++ Drag-N-Drop-Upload/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Drag_N_Drop_Upload
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxUploads = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Drag your img somewhere in the app";
+            // 
+            // listBoxUploads
+            // 
+            this.listBoxUploads.FormattingEnabled = true;
+            this.listBoxUploads.Location = new System.Drawing.Point(4, 28);
+            this.listBoxUploads.Name = "listBoxUploads";
+            this.listBoxUploads.Size = new System.Drawing.Size(173, 134);
+            this.listBoxUploads.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(181, 166);
+            this.Controls.Add(this.listBoxUploads);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Drag-N-Drop";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxUploads;
+    }
+}
+
---
+++ Drag-N-Drop-Upload/MainForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+using System.Net;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+using System.Diagnostics;
+
+namespace Drag_N_Drop_Upload
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            AllowDrop = true;
+            DragEnter += new DragEventHandler(Form1_DragEnter);
+            DragDrop += new DragEventHandler(Form1_DragDrop);
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))// && IsImage((string)e.Data.GetData(DataFormats.FileDrop)))
+            {
+                /*string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                bool allowed = false;
+
+                foreach (string file in files)
+                    if (!IsImage(file))
+                        allowed = false;
+
+                if (allowed)*/
+                    e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach (string filename in files)
+            {
+                //! Looks like shit so just disable it. :)
+                //System.Drawing.Image img = System.Drawing.Image.FromFile(filename);
+                //pictureBox.Image = img;
+
+                using (var w = new WebClient())
+                {
+                    var values = new NameValueCollection
+                    {
+                        {"image", Convert.ToBase64String(File.ReadAllBytes(filename))}
+                    };
+
+                    w.Headers.Add("Authorization", "Client-ID 3290b3fd5c28c93"); //! Client id I got from the API page when registrating the app...
+                    byte[] response = w.UploadValues("https://api.imgur.com/3/upload.xml", values);
+                    string responseStr = Encoding.Default.GetString(response);
+
+                    var xml = XElement.Parse(responseStr);
+
+                    if (xml.Attribute("success").Value == "1")
+                    {
+                        string uploadedURL = xml.Element("link").Value;
+                        Process.Start(uploadedURL);
+                        listBoxUploads.Items.Add(uploadedURL);
+                    }
+                    else
+                        MessageBox.Show("Uploading failed..", "Something went wrong!");
+                }
+            }
+        }
+
+        static bool IsImage(string filename)
+        {
+            try
+            {
+                return System.Drawing.Image.FromFile(filename).RawFormat.Equals(System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            catch (OutOfMemoryException)
+            {
+                return false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
